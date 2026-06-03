578d9fd9fc59d3b9500e9ed8a6cc9f107ebabd6e
Edomocar <edumoralescarlos@gmail.com>
2026-06-03 17:35:38 -0500

    config: parametrizar la URL del backend de forma dinámica mediante variables de entorno

--- next.config.mjs
+++ next.config.mjs
@@ -8,10 +8,11 @@ const nextConfig = {
   },
   devIndicators: false,
   async rewrites() {
+    const backendUrl = process.env.NEXT_PUBLIC_API_URL || 'http://127.0.0.1:8000';
     return [
       {
         source: '/api/:path*',
-        destination: 'http://127.0.0.1:8000/api/:path*',
+        destination: `${backendUrl}/api/:path*`,
       },
     ]
   },
